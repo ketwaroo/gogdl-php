d4704c0a85ac15c10b1955ccb4306b54b6dca3e4
Yaasir Ketwaroo <ketwaroo.yaasir@gmail.com>
2015-05-01 00:39:05 -0400

    added composer for autoloading.

---
+++ config.sample.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+  // copy to `config.php` and edit accordingly
+  // configuration
+ */
+define('GOGPHPDL_BASE_DIR', __DIR__); // working directory
+define('GOGPHPDL_DOWNLOAD_DIR', '/dl/folder'); // download folder
+define('GOGPHPDL_WGET', 'wget'); // wget executable path
+define('GOGPHPDL_USERNAME', 'user@email.com'); // gog.com user
+define('GOGPHPDL_PASSWORD', 'abc123'); // gog.com password
